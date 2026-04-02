243d1d45ef8cd342ecf325c7b723712775eefaec
Divya <divyadarsheel.dds@gmail.com>
2026-04-02 23:26:46 +0545

    feat: navbar partially built, more features remain to be implemented on the navbar

---
+++ js/ui.js
@@ -0,0 +1,15 @@
+// Navbar CSS Starts:
+
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const menu = document.getElementById("menu");
+
+openMenu.addEventListener("click", () => {
+  menu.classList.remove("max-md:w-0");
+  menu.classList.add("max-md:w-full");
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("max-md:w-full");
+  menu.classList.add("max-md:w-0");
+});
